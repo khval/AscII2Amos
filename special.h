282a776d60fa46f50bae067b4d45845b0439dc94
Kjetil Hvalstrand <kjetil@teknobingo.no>
2018-01-07 14:05:21 +0100

    Corrected pointer to function.

--- special.h
+++ special.h
@@ -1,4 +1,7 @@
 
+#ifndef special_h
+#define special_h
+
 char *cmdExit( char *token_buffer, const char **ptr);
 char *cmdFor( char *token_buffer, const char **ptr);
 char *cmdRepeat( char *token_buffer, const char **ptr);
@@ -18,27 +21,10 @@ char *cmdThen( char *token_buffer, const char **ptr);
 
 struct special
 {
-	const char *name,
-	char *fn( char *, const char **);
+	const char *name;
+	char *(*fn) ( char *, const char **);
 };
 
-struct special Special[]
-{
-	{"On",is_,cmdOn},
-	{"Else",is_,cmdElse},
-	{"Data",is_,cmdData},
-	{"ElseIf",is_,cmdElseIf},
-	{"ExitIf",is_,cmdExitIf},
-	{"Then",is_,cmdThen},
-	{"Equ",is_,cmdEqu},
-	{"Lvo",is_,cmdLvo},
-	{"Struc,is_,cmdStruc}
-	{"Procedure",is_,cmdProcedure},
-	{"Proc",is_,cmdProc},
-	{"For",is_,cmdFor},
-	{"Repeat",is_,cmdRepeat},
-	{"While",is_,cmdWhile},
-	{"Until",is_,cmdUntil},
-	{"If",is_,cmdIf},
-	{"Do",is_,cmdDo},
-}
+
+#endif
+

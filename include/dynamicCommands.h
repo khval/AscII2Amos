d15aac827ef235e09cb5a60b3d09dee899673139
Kjetil Hvalstrand <kjetil@teknobingo.no>
2018-01-18 21:35:55 +0100

    Should have included this before, but yes, we need to know what extension the command is.

--- include/dynamicCommands.h
+++ include/dynamicCommands.h
@@ -5,14 +5,14 @@ class DynamicCommand
 
 	public:
 	unsigned short token;
-	int extension;			// if there is a extension number.
+	unsigned short extension;			// if there is a extension number.
 	char *name;
 	int len;
 	int args;
 	int comma;
 	BOOL return_value;
 
-	DynamicCommand(unsigned short token, char *name,int args,BOOL return_value);
+	DynamicCommand(unsigned short token,unsigned short  extension, char *name,int args,BOOL return_value);
 	DynamicCommand();
 	~DynamicCommand();
 };

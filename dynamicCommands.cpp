cbf127fd3f467078083ca91ab3f8fcd855a11459
Kjetil Hvalstrand <kjetil@teknobingo.no>
2018-01-20 21:03:19 +0100

    We need to save args and return value in the command class.

--- dynamicCommands.cpp
+++ dynamicCommands.cpp
@@ -1,14 +1,22 @@
 #include <string.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <proto/exec.h>
 #include <proto/dos.h>
 
 #include "dynamicCommands.h"
 
-DynamicCommand::DynamicCommand(unsigned short _token, unsigned short _extension, char *_name,int args,BOOL return_value)
+DynamicCommand::DynamicCommand(unsigned short _token, unsigned short _extension, char *_name,int _args,BOOL _return_value)
 {
 	token = _token;
 	extension = _extension;
+	args = _args;
+	return_value = _return_value;
+
+	printf("%s%s,%d",
+		return_value  ? "=" : "",
+		_name,
+		args);
 
 	if (_name)
 	{
